0685b8c210c9bb154de636ede04ca9fca8ab0daf
oferfrid <oferfrid@gmai.com>
2019-09-20 15:55:00 +0300

    selection not working yet

--- app_main.py
+++ app_main.py
@@ -8,7 +8,8 @@ class Window(tk.Tk):
     songs_files_list = None
     songs_name_list = None
     songs_list_box = None
-    selected_song_label = None
+    debug_selected_song_label = None
+    debug_search_label = None
 
     def __init__(self):
         tk.Tk.__init__(self)
@@ -17,7 +18,8 @@ class Window(tk.Tk):
         # self.wm_iconbitmap('icon.ico')
         self.init_songs()
         self.init_songsListbox()
-        self.init_song_title()
+        self.init_debug_title()
+        self.current_search_str = []
 
     def init_songs(self):
         fp = get_files_path(r'C:\Users\oferfrid\Downloads\ppt', extantions=['ppt', 'pptx'])
@@ -25,26 +27,53 @@ class Window(tk.Tk):
         self.songs_files_list = sorted_files_path
         self.songs_name_list = sorted_file_names
 
-    def  init_song_title(self):
+    def  init_debug_title(self):
         label = tk.Label(self)
         label.pack()  # grid(row=3, column=3)
-        self.selected_song_label = label
+        self.debug_selected_song_label = label
+        label = tk.Label(self)
+        label.pack()  # grid(row=3, column=3)
+        self.debug_search_label = label
 
     def init_songsListbox(self):
-        listbox = tk.Listbox(self)
+        listbox = tk.Listbox(self, selectmode='SINGLE')
         listbox.pack() #grid(row=3, column=3)
         listbox.bind("<KeyPress>", self.keydown)
         listbox.bind("<KeyRelease>", self.keyup)
         for item in self.songs_name_list:
             listbox.insert(tk.END, item)
-        listbox.select_set(0)
+        listbox.select_set(6)
         listbox.focus_set()
         self.songs_list_box = listbox
 
     def keydown(self,e):
         print('doun', e.char)
+        print('doun', e.keysym)
         if e.keysym == 'Return':
-            self.selected_song_label['text'] = self.songs_files_list[self.songs_list_box.curselection()[0]]
+            self.debug_selected_song_label['text'] = self.songs_files_list[self.songs_list_box.curselection()[0]]
+        elif e.keysym=='Up' or e.keysym=='Down':
+            self.current_search_str = []
+            self.update_search_selection()
+        elif (e.char!=''):
+            # is char
+            if (1488<=ord(e.char)) & (ord(e.char)<=1514):
+                # hebrew char
+                self.current_search_str += [e.char]
+                self.update_search_selection()
+            elif e.char==' ':
+                # space char
+                self.current_search_str += [e.char]
+                self.update_search_selection()
+
+    def update_search_selection(self):
+        search_str = ''.join(self.current_search_str)
+        self.debug_search_label['text'] = search_str
+        if len(search_str)>0:
+            found_indexes = [i for i,t in enumerate(self.songs_name_list) if t.startswith(search_str)]
+            if len(found_indexes)>0:
+                # self.songs_list_box.selection_clear()
+                self.songs_list_box.select_set(found_indexes[0])
+                self.songs_list_box.see(found_indexes[0])
 
     def keyup(self,e):
         print('up', e.char)
